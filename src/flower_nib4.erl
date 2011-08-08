e8854896f223e27124e7ac80e6585c38bb56271e
Andreas Schultz <aschultz@tpip.net>
2011-08-08 20:57:31 +0200

    serialuze nib to list

--- src/flower_nib4.erl
+++ src/flower_nib4.erl
@@ -9,7 +9,7 @@
 
 -module(flower_nib4).
 
--export([new/0, add/3, del/2, lookup/2]).
+-export([new/0, add/3, del/2, lookup/2, to_list/1]).
 
 -record(nib4, {
 		  nib4 :: array()
@@ -33,7 +33,7 @@ add({<<Network:32/integer>>, Mask}, Value, #nib4{nib4 = Nib4} = Nib)
 
 -spec del({Network :: binary(), Mask :: non_neg_integer()}, Nib :: nib4()) -> nib4().
 del({<<Network:32/integer>>, Mask}, #nib4{nib4 = Nib4} = Nib)
-  when Mask > 0, Mask =< 32, is_record(Nib, nib4) ->
+  when Mask >= 0, Mask =< 32, is_record(Nib, nib4) ->
 	N = Network band (16#FFFFFFFF bsl (32 - Mask)),
 	Nib#nib4{nib4 = array:set(Mask, gb_trees:delete_any(N, array:get(Mask, Nib4)), Nib4)}.
 
@@ -49,6 +49,10 @@ lookup(<<IP:32/integer>>, #nib4{nib4 = Nib4} = Nib)
 					   end, none, Nib4);
 
 lookup({<<Network:32/integer>>, Mask}, #nib4{nib4 = Nib4} = Nib)
-  when Mask > 0, Mask =< 32, is_record(Nib, nib4) ->
+  when Mask >= 0, Mask =< 32, is_record(Nib, nib4) ->
 	N = Network band (16#FFFFFFFF bsl (32 - Mask)),
 	gb_trees:lookup(N, array:get(Mask, Nib4)).
+
+to_list(#nib4{nib4 = Nib4} = Nib)
+  when is_record(Nib, nib4) ->
+	array:sparse_foldr(fun(Index, Tree, Acc) -> Acc ++ [{{<<Network:32>>, Index}, Value, Owner} || {Network, {Owner, Value}} <- gb_trees:to_list(Tree)] end, [], Nib4).
